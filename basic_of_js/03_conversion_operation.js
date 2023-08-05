57aa43b688075ddcf20fd17f4dd692324785103c
AbhayKashyap <114595978+Abhay-kashyap@users.noreply.github.com>
2023-08-05 16:35:47 +0000

    03_conversion_operation done

---
+++ basic_of_js/03_conversion_operation.js
@@ -0,0 +1,27 @@
+// let score="hitesh";
+
+// console.log(typeof score);
+// console.log(typeof (score));
+//  let valueInNumber=Number(score);
+//  console.log(typeof valueInNumber);
+//  console.log(valueInNumber);
+//  let value="33abc"//not pure number
+// let check=Number(value);
+// console.log(check);
+// let valuesCheck=true;
+// console.log(typeof valuesCheck);
+// console.log(valuesCheck);
+
+// "33" easy conver =>33
+// "33abc"=>NAN
+// ture => 1; false=>0
+
+let isLoggedIn="";
+let booleanIsLoggedIn=Boolean(isLoggedIn);
+console.log(booleanIsLoggedIn);
+// true=>1 ,false=>0 &&false =>""
+// true=>1,2,3,4,5, && true=>"hitesh"
+let sumNumber =33
+let stringNumber=String(sumNumber);
+console.log(stringNumber);
+console.log(typeof stringNumber);
