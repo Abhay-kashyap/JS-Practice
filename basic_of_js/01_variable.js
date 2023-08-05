e81399905c61ee5f8a87941a3d7c9a31293b65c3
AbhayKashyap <114595978+Abhay-kashyap@users.noreply.github.com>
2023-08-05 11:32:21 +0000

    container config done

---
+++ basic_of_js/01_variable.js
@@ -0,0 +1,14 @@
+const accountId=1445533;
+let accountEmail="abc@gmail.com";
+var accountPassword="14257"
+accountCity="jaipur"
+let accountState;
+// accountId=2  //not allowed
+accountEmail="hchch@gmail.com";
+accountCity="bengaluru";
+accountPassword="12345";
+console.log(accountId);
+/**
+ * prefer not to use var
+ */
+console.table([accountId,accountEmail,accountPassword,accountCity,accountState])
