9f561d5fd3170510a007bebfed05049d47aa29ae
AbhayKashyap <114595978+Abhay-kashyap@users.noreply.github.com>
2023-08-05 12:12:13 +0000

    data_types

---
+++ basic_of_js/data_types.js
@@ -0,0 +1,23 @@
+// "use strict"; //treat all js code as newer version
+// alert(3+3); // we are using nodejs not browser
+console.log(3
+    +3 )// code readability  should high
+
+    console.log('name');
+let name="harsh";
+let age=18;
+let isLoggedIn=false;
+let state;
+// number => 2 to power 53
+// bigInt
+// string =>""
+// boolean =>true/false
+// null => standalone value 
+// undefined means abhi value assign nhi hui hai
+// symbol =>unique
+
+// objects
+console.log(typeof"harsh");
+console.log(typeof age);
+console.log(typeof null)
+console.log(typeof undefined);
