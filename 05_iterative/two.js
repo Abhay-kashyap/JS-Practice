2cbdfc3417de5dfc68693f8afd0437c89148a93e
AbhayKashyap <114595978+Abhay-kashyap@users.noreply.github.com>
2023-08-27 11:13:34 +0000

    while and dowhile understand

---
+++ 05_iterative/two.js
@@ -0,0 +1,19 @@
+
+
+let i=0;
+// while (i<=10) {
+//     console.log(`value of index is ${i} `);
+//     i=i+2
+    
+// }
+
+let myArr=['flash','batman','superman'];
+while (i<myArr.length) {
+    console.log(`the value of array is ${myArr[i]}`);
+    i++;
+}
+let score=1;
+do {
+    console.log(`socre is ${score}`);
+    score++
+} while (score>10);
